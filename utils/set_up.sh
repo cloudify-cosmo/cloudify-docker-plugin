4f0f26966a03179a61e5335d05c8d7e59ac26e8f
Michał Soczewka <michal.soczewka@codilime.com>
2014-08-13 11:18:04 +0200

    More verbose output of the setup script

--- utils/set_up.sh
+++ utils/set_up.sh
@@ -16,7 +16,7 @@ tar -C cloudify_docker_plugin -cf - `ls cloudify_docker_plugin` | vagrant ssh --
 vagrant ssh -- 'cd ~/simple; git clone https://github.com/cloudify-cosmo/cloudify-system-tests.git'
 vagrant ssh -- 'cd ~/simple/cloudify-system-tests; git checkout 3.0rc1 -b 3.0rc1'
 vagrant ssh -- '. ~/cloudify/bin/activate; pip install sh==1.09 path.py==5.1 nose==1.3.3'
-vagrant ssh -- 'cd ~/simple; cp -r cloudify-system-tests/cosmo_tester cloudify_docker_plugin'
+vagrant ssh -- 'cd ~/simple; cp -vr cloudify-system-tests/cosmo_tester cloudify_docker_plugin'
 
 # Install our plugin and use it's docker installation script to be able to
 # execute tests.
