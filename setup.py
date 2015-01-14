74eaa77956cf314ef8ae73df4c883c6b7f33ea5c
Dan Kilman <dankilman@gmail.com>
2015-01-14 09:14:49 +0200

    bump version

--- setup.py
+++ setup.py
@@ -33,14 +33,14 @@ if 'CELERY_WORK_DIR' in os.environ:
 
 setuptools.setup(
     name='cloudify-docker-plugin',
-    version='1.2a2',
+    version='1.2a3',
     author='Gigaspaces',
     author_email='cosmo-admin@gigaspaces.com',
     description='A Cloudify plugin enabling it to create'
                 'and manipulate Docker containers.',
     license='LICENCE',
     install_requires=[
-        'cloudify-plugins-common==3.2a2',
+        'cloudify-plugins-common==3.2a3',
         'docker-py==0.6.0',
     ],
     packages=['docker_plugin'],
