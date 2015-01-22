4e88916a5350853ee7214edf2fc3a779b7ec5e90
earthmant <trammell.earthling@gmail.com>
2015-01-22 10:17:03 +0200

    removing these old tests

--- docker_plugin/tests/unit_tests/test_image_pull.py
+++
@@ -1,8 +0,0 @@
-from docker_plugin import tasks
-import TestWithMockupCtx
-
-
-class TestImagePull(TestWithMockupCtx.TestWithMockupCtx):
-
-    def test_pull(self):
-        tasks.pull(image_pull={'repository': 'uric/nodecellar'})
