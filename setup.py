64f1683c72abdcded3046bfede5d6ebcf11ba369
opencm <limor@gigaspaces.com>
2015-02-25 13:59:15 +0200

    Bump version to 1.2m6

--- setup.py
+++ setup.py
@@ -17,14 +17,14 @@ import setuptools
 setuptools.setup(
 
     name='cloudify-docker-plugin',
-    version='1.2a4',
+    version='1.2a6',
     author='Gigaspaces',
     author_email='cosmo-admin@gigaspaces.com',
     description='A Cloudify plugin enabling it to create'
                 'and manipulate Docker containers.',
     license='LICENCE',
     install_requires=[
-        'cloudify-plugins-common>=3.2a4',
+        'cloudify-plugins-common>=3.2a6',
         'docker-py==0.7.1'
     ],
     packages=['docker_plugin'],
