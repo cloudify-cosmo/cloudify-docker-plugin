2eefe4e99c1ccfc8315744054c479cfccfcc328a
earthmant <earthmant@icloud.com>
2015-04-08 23:18:48 +0300

    tests work

--- system_tests/docker_plugin_test.py
+++ system_tests/docker_plugin_test.py
@@ -13,44 +13,80 @@
 #    * See the License for the specific language governing permissions and
 #    * limitations under the License.
 
-from cosmo_tester.framework.testenv import TestCase
+import os
+import testtools
+
+from fabric.api import settings, run
+from fabric.api import env as fabric_env
+from cloudify.workflows import local
 
 TEST_KEY_NAME = 'docker_test_key'
 TEST_KEY_PATH = '~/.ssh/docker_test_key.pem'
+IGNORED_LOCAL_WORKFLOW_MODULES = (
+    'worker_installer.tasks',
+    'plugin_installer.tasks'
+)
 
 
-class DockerPluginTests(TestCase):
+class DockerPluginTests(testtools.TestCase):
 
     def setUp(self):
         super(DockerPluginTests, self).setUp()
 
-    def _set_up(self):
-        self.blueprint_path = 'resources/blueprint.yaml'
+        self.blueprint_path = \
+            os.path.join(os.path.dirname(__file__),
+                         'resources', 'blueprint.yaml')
 
         inputs = {
-            'aws_access_key_id': self.env.aws_access_key_id,
-            'aws_secret_access_key': self.env.aws_secret_access_key,
-            'current_ip': self._get_current_machine_ip(),
-            'image': self.env.ubuntu_image_name,
-            'size': self.env.medium_instance_type,
+            'aws_access_key_id': None,
+            'aws_secret_access_key': None,
+            'current_ip': '0.0.0.0/0',
+            'image': 'ami-3cf8b154',
+            'size': 'm3.medium',
             'key_name': TEST_KEY_NAME,
-            'private_key_path': TEST_KEY_PATH
+            'private_key_path': TEST_KEY_PATH,
+            'core_version': '3.2m8',
+            'plugins_version': '1.2m8',
+            'agent_user': 'ubuntu'
         }
 
-    def _run_local_install_workflow(self):
-        pass
+        self.env = local.init_env(
+            self.blueprint_path, name=self._testMethodName,
+            inputs=inputs,
+            ignored_modules=IGNORED_LOCAL_WORKFLOW_MODULES)
 
-    def _run_local_uninstall_workflow(self):
-        pass
+    def tearDown(self):
+        super(DockerPluginTests, self).tearDown()
+        self.env.execute('uninstall', task_retries=10)
 
-    def _install_virtual_env_remotely(self, elastic_ip):
-        pass
+    def test_plugin(self):
 
-    def _install_docker_plugin_remotely(self, elastic_ip):
-        pass
+        self.env.execute('install', task_retries=10)
 
-    def _run_nosetests_remotely(self, elastic_ip, path_to_tests_directory):
-        pass
+        for node in self.env.storage.get_nodes():
+            if 'docker_system_test_key_pair' in node.id:
+               keypair = node
 
-    def _get_current_machine_ip(self):
-        pass
+        for node_instance in self.env.storage.get_node_instances():
+            if 'docker_system_test_host' in node_instance.node_id:
+                host = node_instance
+
+        if not keypair or not host:
+            raise Exception(
+                'Keypair {0} or host {0} cannot be None.'
+                .format(keypair, host))
+
+        fabric_env = {
+            'user': 'ubuntu',
+            'key_filename': keypair.properties['private_key_path'],
+            'host_string': host.runtime_properties['public_ip_address']
+        }
+
+        command = 'nosetests --with-cov --cov-report term-missing ' \
+                  '--cov docker_system_test/docker_plugin ' \
+                  'docker_system_test/docker_plugin/tests'
+
+        with settings(**fabric_env):
+            result = run(command)
+
+        self.assertIn('OK', result)
