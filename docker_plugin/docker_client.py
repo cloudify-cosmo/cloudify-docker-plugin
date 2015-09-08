495fed62de356c01c1ad4d70b2d299ca1f34e63f
Denis Makogon <Denys@gigaspaces.com>
2015-09-08 11:51:11 +0300

    Make plugin non-host-agent
    Plugin uses Docker client that is able
    to connect to any Docker service, so,
    no need to keep this plugin as host agent plugin due

--- docker_plugin/docker_client.py
+++ docker_plugin/docker_client.py
@@ -18,6 +18,7 @@ from docker.client import Client
 from docker.errors import DockerException
 
 # Cloudify Imports
+from cloudify import ctx
 from cloudify.exceptions import NonRecoverableError
 
 
@@ -33,7 +34,23 @@ def get_client(daemon_client):
     """
 
     try:
-        return Client(**daemon_client)
+        ctx.logger.info("Daemom connection options: {0}.".format(
+            str(daemon_client)))
+        tp = daemon_client.get('transport_protocol', 'tcp')
+        docker_host = daemon_client.get('host_string', 'localhost')
+        docker_port = daemon_client.get('host_port', 2375)
+
+        base_url = "{0}://{1}:{2}".format(tp, docker_host, docker_port)
+        version = str(daemon_client.get('api_version', '1.19'))
+        connection_timeout = int(
+            daemon_client.get('connection_timeout', 60))
+        use_tls = daemon_client.get('enable_tls', False)
+
+        return Client(base_url=base_url,
+                      version=version,
+                      tls=use_tls,
+                      timeout=connection_timeout)
+
     except DockerException as e:
         raise NonRecoverableError(
             'Error while getting client: {0}.'.format(str(e)))
