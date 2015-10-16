d94542172726bc239377923c505f13c7fb3d32c4
mailjainrahul <mailjainrahul@users.noreply.github.com>
2015-10-16 14:04:46 -0400

    Updated setup.py
    Updated setup.py with the newer version of docker-py

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setuptools.setup(
     license='LICENCE',
     install_requires=[
         'cloudify-plugins-common>=3.3a7',
-        'docker-py==1.2.3'
+        'docker-py==1.4.0'
     ],
     packages=['docker_plugin'],
     zip_safe=False
