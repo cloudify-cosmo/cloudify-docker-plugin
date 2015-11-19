f672965da09b1db2435dbdd330def6457ceec40b
ran <ran@gigaspaces.com>
2015-11-19 17:55:48 +0200

    refactored docker plugin test

--- system_tests/manager/test_docker_plugin.py
+++ system_tests/manager/test_docker_plugin.py
@@ -38,9 +38,8 @@ class TestDockerPlugin(TestCase):
                          'resources', 'blueprint.yaml')
 
         if self.env.install_plugins:
-            cfy.local(
-                'install-plugins',
-                blueprint_path=self.blueprint_path).wait()
+            self.cfy.install_plugins_locally(
+                blueprint_path=self.blueprint_path)
 
         inputs = {
             'current_ip': '0.0.0.0/0',
