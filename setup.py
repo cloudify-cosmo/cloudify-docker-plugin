df34ad3284d148c9da8b738012bdf88daf03ec64
EarthmanT <EarthmanT@users.noreply.github.com>
2023-11-30 14:27:03 -0500

    requirements

--- setup.py
+++ setup.py
@@ -16,8 +16,9 @@
 
 import os
 import re
+import sys
 import pathlib
-from setuptools import setup
+from setuptools import setup, find_packages
 
 
 def get_version():
@@ -28,25 +29,33 @@ def get_version():
         return re.search(r'\d+.\d+.\d+', var).group()
 
 
-setup(
+install_requires = [
+    'docker>=5.0.3', # Latest with official support for python 3.6 is 5.0.3
+    'fabric>=2.5.0',
+    'patchwork>=1.0.1'  # to copy files to docker machine
+]
 
+if sys.version_info.major == 3 and sys.version_info.minor == 6:
+    packages = ['cloudify_docker']
+    install_requires += [
+    'cloudify-common>=6.4,<7.0',
+    'cloudify-utilities-plugins-sdk>=0.0.61',  # Shared Resource Downloader
+    ]
+else:
+    packages = find_packages()
+    install_requires += [
+        'fusion-common',
+        'cloudify-utilities-plugins-sdk'
+    ]
+
+setup(
     name='cloudify-docker-plugin',
     version=get_version(),
     author='Cloudify Platform LTD',
     author_email='hello@cloudify.co',
     description='Manage Docker nodes/containers by Cloudify.',
-    packages=['cloudify_docker'],
+    packages=packages,
     license='LICENSE',
     zip_safe=False,
-    install_requires=[
-        "cloudify-common>=6.4",
-        "docker>=5.0.3", # Latest with official support for python 3.6 is 5.0.3
-        "cloudify-utilities-plugins-sdk>=0.0.61",  # Shared Resource Downloader
-        "fabric>=2.5.0",
-        "patchwork>=1.0.1"  # to copy files to docker machine
-    ],
-    test_requires=[
-        "cloudify-common>=6.4",
-        "docker>=5.0.3",
-    ]
+    install_requires=install_requires,
 )
